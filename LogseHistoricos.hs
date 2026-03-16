5e52cd68ded65ff8d544c1ac97b3fcab2dc3ae32
joa1paulo <72257494+joa1paulo@users.noreply.github.com>
2026-03-16 13:47:22 +0000

    arrumando arquivos e iniciando a implementação de logs e históricos

--- LogseHistoricos.hs
+++ LogseHistoricos.hs
@@ -2,3 +2,8 @@ module LogseHistoricos where
 
 import Estruturas
 
+-- LOGS E HISTORICOS
+-- [ ] Persistência do Log de Operações: Criar a função para salvar e ler o histórico de operações (cadastros, empréstimos, erros) em arquivo, garantindo a rastreabilidade exigida.
+-- [ ] Persistência do Histórico de Alterações: Implementar o salvamento das edições de itens/usuários (antes vs. depois) para fins de auditoria.
+-- [ ] Integração com o Banco de Dados: Garantir que a função importarCSV preencha corretamente o tipo BancoDeDados, incluindo as listas de empréstimos e logs (atualmente estão retornando listas vazias no seu código).
+
